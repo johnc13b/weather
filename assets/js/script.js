fd13ccf8d9d10e182aef4bd2dd4303938151cf85
johnc13b <100248387+johnc13b@users.noreply.github.com>
2022-04-24 23:46:54 -0400

    cart api calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,45 @@
 
-var apiKey = "f4ca63fe60c915639b5226ae9b65c99f";
-var searchCity = document.getElementById("search-city");
+var owAPI = "f4ca63fe60c915639b5226ae9b65c99f";
+var searchCity = document.getElementById("searchBox");
 var searchButton = document.getElementById("search-button");
 var currentWeather = document.getElementById("current-weather");
 var currentDate = document.getElementById("date");
 var cityName = document.getElementById("cityLocation");
-var temp = document.getElementById("temperature");
-var wind = document.getElementById("wind");
-var humidity = document.getElementById("humidity");
+var currentTemp = document.getElementById("temperature");
+var currentWind = document.getElementById("wind");
+var currentHumidity = document.getElementById("humidity");
+var currentUV = document.getElementById("uvIndex");
 
-var formSubmit = function(event) {
-    event.preventDefault();
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
-    
-}
+searchButton.addEventListener('click', function () {
+    const search = searchCity.value.trim();
+    console.log(search);
+    history();
+    weatherLookup(search);
+    weatherForecast(search);
+});
+
+function weatherLookup() {
+    let api = "https://api.openweathermap.org/data/2.5/weather?q=" + cityLocation + "&appid" + owAPI;
+    fetch(api).then(function(response) {
+        console.log(response);
+        response.json().then(function(data) {
+            console.log(data);
+            displayWeather(data);
+});
+})}
+
+function displayWeather() {
+
+};
+
+function futureWeather() {
+    let apiCast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityLocation + "&units=imperial&appid=" + owAPI;
+    fetch(apiCast).then(async function(response) {
+        const data = await response.json();
+        console.log(data);
+})
+};
+
+
